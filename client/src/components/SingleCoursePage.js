32e6c50ae59f257b300e671b2cc9a54bc8057643
katrinap77 <104406554+katrinap77@users.noreply.github.com>
2023-03-22 11:28:59 +1300

    added

--- client/src/components/SingleCoursePage.js
+++ client/src/components/SingleCoursePage.js
@@ -10,7 +10,7 @@ const SingleCoursePage = () => {
     const fetchData = async () => {
       // const response = await fetch("http://localhost:5001/courses/${id}");
       const response = await fetch(
-        `https://api-bfxv.onrender.com/courses/${id}`,
+        `https://my-json-server.typicode.com/katrinap77/jsoncourses/courses/${id}`,
         {
           method: "GET",
           headers: {
